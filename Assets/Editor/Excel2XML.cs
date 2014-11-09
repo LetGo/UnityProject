d92ebfdd24a3582e8fa2914a71c0f0a85604e809
LetGo <zcxzcx168>
2014-11-09 22:15:11 +0800

    添加excelToXML 工具

---
+++ Assets/Editor/Excel2XML.cs
@@ -0,0 +1,261 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using Excel;
+using System.Data;
+using UniCommon;
+using System;
+using System.Text;
+
+public class Excel2XML : EditorWindow
+{
+    static string m_strRersourcePath;
+    static string m_strTargetPath;
+    static List<FileInfo> m_lstFiles = new List<FileInfo>();
+    static bool m_bDataListing = false;
+    static bool[] m_bLstEceleFileCheck;
+    static string[] expectStr ;
+    static int[] expectNum;
+    static Vector2 m_v2ScrollPos = Vector2.zero;
+
+    [MenuItem("GameTool/ExcelToXML New")]
+    static void Init()
+    {
+        Excel2XML window = (Excel2XML)EditorWindow.GetWindow(typeof(Excel2XML));
+        window.Show();
+        m_strRersourcePath = PlayerPrefs.GetString("exelePath");
+        m_strTargetPath = PlayerPrefs.GetString("savePath");
+        if (!string.IsNullOrEmpty(m_strRersourcePath))
+        {
+            LoadData(m_strRersourcePath);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Width(80.0f);
+        GUILayout.Label("源文件路径：" + m_strRersourcePath, GUILayout.Height(15));
+        if (GUILayout.Button("选择源文件路径", GUILayout.Height(30)))
+        {
+            string newPath = string.Empty;
+            newPath = EditorUtility.OpenFolderPanel("选择源文件路径", m_strRersourcePath, "");
+            if (!string.IsNullOrEmpty(newPath) && !newPath.Equals(m_strRersourcePath))
+            {
+                m_strRersourcePath = newPath;
+                PlayerPrefs.SetString("exelePath", m_strRersourcePath);
+            }
+        }
+
+
+        GUILayout.Label("保存文件路径：" + m_strTargetPath, GUILayout.Height(15));
+        if (GUILayout.Button("保存文件路径", GUILayout.Height(30)))
+        {
+            string newPath = string.Empty;
+            newPath = EditorUtility.OpenFolderPanel("选择保存路径", m_strRersourcePath, "");
+            if (!newPath.Equals(m_strTargetPath) && !string.IsNullOrEmpty(newPath))
+            {
+                m_strTargetPath = newPath;
+                PlayerPrefs.SetString("savePath", m_strTargetPath);
+            }
+        }
+
+        if (GUILayout.Button("确认选择", GUILayout.Height(30)))
+        {
+            LoadData(m_strRersourcePath);
+        }
+        SelectEceleData();
+    }
+
+    void XLSX()
+    {
+        string path = Application.dataPath + "/test.xlsx";
+        FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+        DataSet result = excelReader.AsDataSet();
+        if (result == null)
+        {
+            Debug.Log("path =====" + path);
+            return;
+        }
+        int columns = result.Tables[0].Columns.Count;
+        int rows = result.Tables[0].Rows.Count;
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                string nvalue = result.Tables[0].Rows[i][j].ToString();
+                Debug.Log(nvalue);
+            }
+        }
+    }
+
+    static void LoadData(string path)
+    {
+        m_lstFiles.Clear();
+        var directoryInfo = new DirectoryInfo(path);
+        GetFiles(directoryInfo, ref m_lstFiles);
+        //m_lstFiles.Sort();
+        Debug.Log("files :" + m_lstFiles.Count);
+        m_bLstEceleFileCheck = new bool[m_lstFiles.Count];
+        expectNum = new int[m_lstFiles.Count];
+    }
+
+
+    /// <summary>
+    /// 获取execle文件
+    /// </summary>
+    /// <param name="di"></param>
+    /// <param name="files"></param>
+    static void GetFiles(DirectoryInfo di, ref List<FileInfo> files)
+    {
+        foreach (FileInfo file in di.GetFiles())
+        {
+            //获取扩展名
+            if (Path.GetExtension(file.FullName).Equals(".xlsx") || Path.GetExtension(file.FullName).Equals(".xls"))
+            {
+                files.Add(file);
+            }
+        }
+
+        //遍历子文件夹
+        DirectoryInfo[] dis = di.GetDirectories();
+        foreach (DirectoryInfo sdi in dis)
+        {
+            GetFiles(sdi, ref files);
+        }
+    }
+
+    bool update = false;
+    void SelectEceleData()
+    {
+        m_bDataListing = EditorGUILayout.Foldout(m_bDataListing, "Exele配置文件");
+        m_v2ScrollPos = GUILayout.BeginScrollView(m_v2ScrollPos);
+        if (m_bDataListing)
+        {
+            
+            for (int i = 0; i < m_lstFiles.Count; ++i)
+            {
+                EditorGUILayout.BeginHorizontal(GUILayout.Width(400));
+                m_bLstEceleFileCheck[i] = GUILayout.Toggle(m_bLstEceleFileCheck[i], "", GUILayout.Width(30));
+                GUILayout.TextArea(m_lstFiles[i].Name, GUILayout.Width(100));
+
+                expectNum[i] = EditorGUILayout.IntField("不输出项", expectNum[i]);
+
+                if (GUILayout.Button("确定", GUILayout.Width(50)))
+                {
+                    Debug.Log("count :" + expectNum[i]);
+                }
+
+                if (GUILayout.Button("导出", GUILayout.Width(50)))
+                {
+                    ReadExeleData(m_lstFiles[i].Name);
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+        }
+        GUILayout.EndScrollView();
+
+    }
+
+
+    string ReadExeleData(string fileName)
+    {
+        if (string.IsNullOrEmpty(m_strTargetPath))
+        {
+            EditorUtility.DisplayDialog("提示", "保存路径不能为空啊!", "Get it");
+            return "";
+        }
+
+        string fullPath = m_strRersourcePath + "/" + fileName;
+
+        Debug.Log("fullPath :" + fullPath);
+
+        FileStream stream = File.Open(fullPath,FileMode.Open,FileAccess.Read);
+
+        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        if (excelReader == null)
+        {
+            Debug.LogError("excelReader is null");
+            return "";
+        }
+       
+        DataSet result = excelReader.AsDataSet();
+        if (result == null)
+        {
+            Debug.LogError("DataSet is null");
+            return "";
+        }
+
+        DataTable table0 = result.Tables[0];
+
+        int rowCount = table0.Rows.Count; //行
+        int colCount = table0.Columns.Count;//列
+        
+        Debug.Log("col:" + colCount + "row :" + rowCount);
+
+        int index = fileName.LastIndexOf(".");
+        string tabelName = string.Empty;
+        if (index != -1)
+        {
+            tabelName = fileName.Substring(0, index);
+        }
+        
+        List<string> attributeTitleList = new List<string>();
+        for (int i = 0; i < colCount; ++i )
+        {
+            attributeTitleList.Add(table0.Rows[0][i].ToString());
+            //Debug.Log(i +" : "+ table0.Rows[0][i].ToString());
+        }
+
+        List<string> attributeList = new List<string>();
+        attributeList.Clear();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 1; i < rowCount; i++)
+        {
+            builder.Append("\t<").Append(tabelName).Append(" ");
+            for (int j = 0; j < colCount; j++)
+            {
+                string nvalue = table0.Rows[i][j].ToString();
+                if (!string.IsNullOrEmpty(nvalue))
+                {
+                    builder.Append(string.Format("{0}=\"{1}\"{2}", attributeTitleList[j], nvalue, j == colCount - 1 ? "" : " "));
+                }
+            }
+            builder.Append("/>");
+            attributeList.Add(builder.ToString());
+            builder.Length = 0;
+        }
+
+        return Write(tabelName, attributeList);
+    }
+
+    string Write(string name,List<string> attributeList)
+    {
+        string strStart = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\" ?><Root>";
+        string strEnd = "</Root>";
+        string strTargetPath = m_strTargetPath + "/" + name +".xml";
+        Debug.Log("strTargetPath :" + strTargetPath);
+        if (File.Exists(strTargetPath))
+        {
+            File.Delete(strTargetPath);
+        }
+        FileStream stream = new FileStream(strTargetPath,FileMode.Create,FileAccess.Write);
+        StreamWriter sw = new StreamWriter(stream);
+        sw.WriteLine(strStart);
+        attributeList.ApplyAll( C => sw.WriteLine(C) );
+        sw.WriteLine(strEnd);
+        sw.Close();
+        stream.Close();
+        return strTargetPath;
+    }
+}
+
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,258 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UniCommon;
+
+public class UIManager :Singleton<UIManager>
+{
+    //当待删除列表超过这个数，整体destroy 在GC
+    const int MAX_PANEL_TODESTROY = 4;
+    //加入到待删除列表的时间，超过就加入m_lstReadyDestroyPanel
+    const float DESTROY_TIME = 10.0f;
+
+    private UIMgrComponent m_UIMgrComponent;
+    //窗口列表
+    private Dictionary<UIPanelID, GameUIPanelBase> m_dicPanelUI = null;
+    
+    //每个窗口关掉的时间累计
+    private Dictionary<UIPanelID, float> m_dicPanelUIDisableTimeCount = null;
+
+    //待删除列表 当窗口关闭时加入此列表，当次列表打到一定数量一起destroy，如果中途打开则移除
+    private List<UIPanelID> m_lstReadyDestroyPanel = null;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        m_dicPanelUI =  new Dictionary<UIPanelID, GameUIPanelBase>();
+        m_dicPanelUIDisableTimeCount = new Dictionary<UIPanelID, float>();
+        m_lstReadyDestroyPanel = new List<UIPanelID>();
+    }
+
+    public override void UnInitialize()
+    {
+        DestroyAllUI(true);
+
+        m_dicPanelUI.Clear();
+        m_dicPanelUI = null;
+
+        m_dicPanelUIDisableTimeCount.Clear();
+        m_dicPanelUIDisableTimeCount = null;
+
+        m_lstReadyDestroyPanel.Clear();
+        m_lstReadyDestroyPanel = null;
+
+       
+        base.UnInitialize();
+    }
+
+    public void Update()
+    {
+
+    }
+
+
+    public GameUIPanelBase OpenUIpanel(UIPanelID panelID)
+    {
+        GameUIPanelBase panel = null;
+        if (ContainUI(panelID))
+        {
+            panel = m_dicPanelUI[panelID];
+            RemoveReadyDestroyUI(panelID,true);
+        } 
+        else
+        {
+            //第一次打开执行第一次打开只执行一次的函数
+            panel = InstanceUIPanel(panelID, true);
+            if(panel != null) panel.InitOnce();
+        }
+
+        if (panel != null)
+        {
+            panel.OnOpen();
+        }
+
+        return panel;
+    }
+
+    public void CloseUIPanel(UIPanelID id, bool bDestoryNow = false)
+    {
+        if (ContainUI(id))
+        {
+             GameUIPanelBase panel = m_dicPanelUI[id];
+            panel.OnClose();
+            if (bDestoryNow)
+            {
+                GameObject.Destroy(panel.gameObject);
+                m_dicPanelUI.Remove(id);
+                RemoveReadyDestroyUI(id, true);
+            } 
+            else
+            {
+                panel.gameObject.SetActive(false);
+                AddToReadyDestroyUI(id);
+            }
+        } 
+    }
+
+    /// <summary>
+    /// 发送消息到指定窗口
+    /// </summary>
+    /// <param name="panelid"></param>
+    /// <param name="msgID"></param>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public bool SendMsgToUIPanel(UIPanelID panelid,PanelMsgID msgID,object obj = null)
+    {
+        bool isDeal = false;
+        if (ContainUI(panelid))
+        {
+            isDeal = m_dicPanelUI[panelid].OnMsg(msgID,obj);
+        } 
+        else
+        {
+            Debug.LogWarning("there is not UIPanel which ID is " + (int)panelid);
+        }
+        return isDeal;
+    }
+
+    bool ContainUI(UIPanelID panelid)
+    {
+        return m_dicPanelUI.ContainsKey(panelid);
+    }
+
+    /// <summary>
+    /// 加入计时列表
+    /// </summary>
+    /// <param name="id"></param>
+    private void AddToReadyDestroyUI(UIPanelID id)
+    {
+        if (!m_dicPanelUIDisableTimeCount.ContainsKey(id))
+        {
+            m_dicPanelUIDisableTimeCount.Add(id, Time.realtimeSinceStartup);
+        } 
+        else
+        {
+            //从新计时
+            m_dicPanelUIDisableTimeCount[id] = Time.realtimeSinceStartup;
+        }
+
+        int index = m_lstReadyDestroyPanel.IndexOf(id);
+        if (index != -1)
+        {
+            m_lstReadyDestroyPanel.RemoveAt(index);
+        }
+    }
+
+    /// <summary>
+    /// 移除计时列表
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="bRemove"> 从待删除列表移除</param>
+    private void RemoveReadyDestroyUI(UIPanelID id, bool bRemove)
+    {
+        if (m_dicPanelUIDisableTimeCount.ContainsKey(id))
+        {
+            m_dicPanelUIDisableTimeCount.Remove(id);
+            if (!bRemove) return;
+
+            int index = m_lstReadyDestroyPanel.IndexOf(id);
+            if (index != -1)
+            {
+                m_lstReadyDestroyPanel.RemoveAt(index);
+            }
+        }
+    }
+
+    private GameUIPanelBase InstanceUIPanel(UIPanelID id, bool bShowWhileCreat)
+    {
+        //从配置获取资源路径 todo
+        string resPath = "";
+
+        GameObject go = m_UIMgrComponent.CenterAnchor.AddGameObject(resPath,
+            Vector3.zero,Vector3.one,Quaternion.identity,true);
+
+        GameUIPanelBase panel = go.GetComponent<GameUIPanelBase>();
+        if (panel == null)
+        {
+            Debug.LogError("can not find GameUIPanelBase Component in panel which id is :" + id);
+            return null;
+        }
+
+        m_dicPanelUI.Add(id,panel);
+
+        return panel;
+    }
+
+    IEnumerator EndOfFrameUnloadAssets()
+    {
+        yield return new WaitForEndOfFrame();
+        Resources.UnloadUnusedAssets();
+        System.GC.Collect();
+    }
+
+    void DestroyNoUseUI()
+    {
+        if (Time.frameCount % 10 == 0 || m_dicPanelUIDisableTimeCount.Count == 0)
+        {
+            return;
+        }
+
+        //将关闭时间超过DESTROY_TIME 的panle加入待删除列表
+        float time = Time.realtimeSinceStartup;
+        foreach (var key in m_dicPanelUIDisableTimeCount.Keys)
+        {
+            if (time - m_dicPanelUIDisableTimeCount[key] >= DESTROY_TIME && !m_lstReadyDestroyPanel.Contains(key) )
+            {
+                m_lstReadyDestroyPanel.Add(key);
+            }
+        }
+
+        //检测待删除列表是否超过或等于最大值 超过就删除
+        if (m_lstReadyDestroyPanel.Count >= MAX_PANEL_TODESTROY)
+        {
+            foreach (var key in m_lstReadyDestroyPanel)
+            {
+                if (m_dicPanelUI.ContainsKey(key))
+                {
+                    GameObject.Destroy(m_dicPanelUI[key].gameObject);
+                    m_dicPanelUI.Remove(key);
+                }
+                RemoveReadyDestroyUI(key, false);
+            }
+            m_lstReadyDestroyPanel.Clear();
+            GameManager.Instance.UnLoadUnuseAsset();
+        }
+    }
+
+    public GameUIPanelBase GetPanelByID(UIPanelID id)
+    {
+        if (ContainUI(id))
+        {
+            return m_dicPanelUI[id];
+        }
+        return null;
+    }
+
+    public void DestroyAllUI(bool bRemoveAsset)
+    {
+        foreach ( KeyValuePair<UIPanelID,GameUIPanelBase> kvPair in m_dicPanelUI)
+        {
+           
+            if (kvPair.Value != null)
+            {
+                kvPair.Value.OnClose();
+                GameObject.Destroy(kvPair.Value.gameObject);
+            }
+           
+        }
+        
+        m_dicPanelUI.Clear();
+        m_dicPanelUIDisableTimeCount.Clear();
+        m_lstReadyDestroyPanel.Clear();
+
+        if (bRemoveAsset)
+        {
+            Resources.UnloadUnusedAssets();
+        }
+    }
+}
