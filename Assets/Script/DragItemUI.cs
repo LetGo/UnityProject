bf5f9e910463970013db93bec4b85eaa9979fb94
LetGo <369800228@qq.com>
2014-11-11 15:21:34 +0800

    拖动界面item

---
+++ Assets/Script/DragItemUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DragItemUI : MonoBehaviour {
+
+	public List<Gameobject> m_listDragItem = new List<GameObject>();
+	public GameObject m_goDragItemRoot;
+	public UIScrollview
+}
