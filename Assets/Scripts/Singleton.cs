f3967fc977a7e0ad10ae29a40caa84404954e7b2
LetGo <369800228@qq.com>
2014-12-28 17:09:43 +0800

    //

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
-
+//class 类约束指定类型T必须是引用类型
+//new() 这是一个构造函数约束,指定类型T必须有一个默认构造函数
 public class Singleton<T> where T :class, new()
 {
     private static T _instance;
