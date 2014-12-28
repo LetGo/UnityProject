7a053cc965fb4a2ccb9742d2c3df8674e35ce2df
LetGo <369800228@qq.com>
2014-12-28 18:23:57 +0800

    skill editor

--- Assets/Editor/SkillEditor/SkillEditorWindow.cs
+++ Assets/Editor/SkillEditor/SkillEditorWindow.cs
@@ -8,16 +8,51 @@ namespace SkillEditor{
 	public class SkillEditorWindow : EditorWindow {
 		static SkillEditorWindow Instance = null;
 
+		public int RoleIndex = 0;
+		public int RolePreAction = 0;
+		public int RoleAttackActiom = 0;
+		string[] roleModels = new string[1];
+		string[] roleModelAnimation = new string[1];
+
+		Vector2 scrollPos = Vector2.zero;
+		float modelActionSlider = 0;
 		[MenuItem("GameTool/SkillEditor %E")]
 		static void Init(){
-			if(EditorApplication.isPlaying && !EditorApplication.isPaused)
-				Instance = EditorWindow.GetWindow<SkillEditorWindow> ("技能编辑器", false, typeof(SkillEditorWindow));
+			if (EditorApplication.isPlaying && !EditorApplication.isPaused) {
+				Instance = EditorWindow.GetWindow<SkillEditorWindow> ("技能编辑器", false, typeof(SkillEditorWindow));	
+				GetAllModels();
+			}
 			else
 				Debug.Log("运行场景");
 		}
 
-		void OnGUI(){
+		static void GetAllModels(){
+					
+		}
 
+		void OnGUI(){
+			scrollPos = EditorGUILayout.BeginScrollView (scrollPos);
+
+			RoleIndex = EditorGUILayout.Popup ("角色模型", RoleIndex, roleModels);
+
+			EditorGUILayout.BeginHorizontal ();
+			if (GUILayout.Button ("添加模型")) {
+				//TODO roleLoader.load
+			}
+			if (GUILayout.Button ("删除模型")) {
+				//TODO roleLoader.delete
+			}
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal ();
+			RolePreAction = EditorGUILayout.Popup ("准备动作", RolePreAction, roleModelAnimation);
+			RoleAttackActiom = EditorGUILayout.Popup ("攻击动作", RoleAttackActiom, roleModelAnimation);
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.LabelField("调整模型动作");
+			modelActionSlider = EditorGUILayout.Slider (modelActionSlider, 0, 1);
+
+			EditorGUILayout.EndScrollView ();
 		}
 	}
 }
