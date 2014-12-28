f285491fc079b9e25e7012e28bbaac84aa88e510
LetGo <369800228@qq.com>
2014-12-28 15:12:59 +0800

    s

---
+++ Assets/Editor/SkillEditor/SkillEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+
+namespace SkillEditor{
+	public class SkillEditorWindow : EditorWindow {
+		static SkillEditorWindow Instance = null;
+
+		[MenuItem("GameTool/SkillEditor %E")]
+		static void Init(){
+			if(EditorApplication.isPlaying && !EditorApplication.isPaused)
+				Instance = EditorWindow.GetWindow<SkillEditorWindow> ("技能编辑器", false, typeof(SkillEditorWindow));
+			else
+				Debug.Log("运行场景");
+		}
+
+		void OnGUI(){
+
+		}
+	}
+}
