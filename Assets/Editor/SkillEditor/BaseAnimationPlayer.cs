17333a2254fc54f90bca1cd69f42bb6d62518c22
LetGo <369800228@qq.com>
2014-12-31 14:41:18 +0800

    add

---
+++ Assets/Editor/SkillEditor/BaseAnimationPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAnimationPlayer  {
+
+	public SkillBean skillBean{ get; set;}
+	public GameObject roleObj{ get; set;}
+	Animation roleAnimation{ get; set;}
+
+	protected string currentAnimationClip = string.Empty;
+
+	public virtual void Play(){
+
+	}
+
+	public virtual void Stop(){
+
+	}
+
+	public virtual void Pause(){
+
+	}
+
+	public virtual void Resume(){
+
+	}
+}
