4b38e077897a5d9be4ee37015a327a64a1b98986
LetGo <zcxzcx168>
2015-01-16 00:36:55 +0800

    A

---
+++ Assets/Scripts/DataBase/Heros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+public class Heros{
+	public int ID;
+	public System.String Name;
+	public System.String Model;
+	public List<int> Skills = new List<int>();
+	public int AttackSpeed;
+	public int Attack;
+	public int HP;
+}
